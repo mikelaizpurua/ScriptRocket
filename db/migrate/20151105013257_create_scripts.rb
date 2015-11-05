f4cb9e46dab2ec8b4285b591ecdf600e99c0ffa5
David Turk <david.p.turk@gmail.com>
2015-11-05 10:27:18 -0500

    Removed carrierwave gem from project

--- db/migrate/20151105013257_create_scripts.rb
+++
@@ -1,9 +0,0 @@
-class CreateScripts < ActiveRecord::Migration
-  def change
-    create_table :scripts do |t|
-      t.string :description
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20151105014513_add_user_id_to_scripts.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToScripts < ActiveRecord::Migration
-  def change
-    add_column :scripts, :user_id, :integer
-    add_index :scripts, :user_id
-  end
-end
