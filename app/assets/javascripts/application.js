c9e5bdf12fceaf60b8bf2e56bbe5f6c6de59ce97
David Turk <david.p.turk@gmail.com>
2015-11-08 14:21:42 -0500

    Added Parrallax effect on login pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,3 +38,14 @@ $(document).on('ready page:load', function() {
 });
 
 
+$(document).ready(function(){
+  $(document).mousemove(function(e){
+     TweenLite.to($('#signinbody'), 
+        .5, 
+        { css: 
+            {
+                backgroundPosition: ""+ parseInt(event.pageX/8) + "px "+parseInt(event.pageY/'12')+"px, "+parseInt(event.pageX/'15')+"px "+parseInt(event.pageY/'15')+"px, "+parseInt(event.pageX/'30')+"px "+parseInt(event.pageY/'30')+"px"
+            }
+        });
+  });
+});
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -78,7 +78,6 @@ class LinksController < ApplicationController
     redirect_to :back    
   end
 
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_link
