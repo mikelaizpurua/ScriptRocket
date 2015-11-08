5bdfa3b41cf019bdc94fec1096140819b52c5749
Mikel Aizpurua <mikel@aizpurua.me>
2015-11-08 14:39:37 -0500

    commit after migrating Profile model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106154852) do
+ActiveRecord::Schema.define(version: 20151108193417) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -42,6 +42,29 @@ ActiveRecord::Schema.define(version: 20151106154852) do
   end
 
   add_index "links", ["integer"], name: "index_links_on_integer"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "gender"
+    t.string   "city"
+    t.string   "occupation"
+    t.date     "birthdate"
+    t.integer  "user_id"
+    t.boolean  "comedy"
+    t.boolean  "adventure"
+    t.boolean  "drama"
+    t.boolean  "action"
+    t.boolean  "thriller"
+    t.boolean  "horror"
+    t.boolean  "romantic_comedy"
+    t.boolean  "musical"
+    t.boolean  "documentary"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
