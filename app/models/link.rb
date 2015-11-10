af05b025e244bc4d6188c7c9df361e8ee7eaa92f
Mikel Aizpurua <mikel@aizpurua.me>
2015-11-10 11:35:20 -0500

    working with PDFreader within index and show page"

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,4 +8,3 @@ class Link < ActiveRecord::Base
     has_attached_file :image, :styles => { :medium => "300x300>", thumbnail: "60x60" }
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf"]
 end
-
