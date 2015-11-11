1f0a70ca32edfd69aea4fb72b754202c060fb560
Madelene Campos <madelenecampos@gmail.com>
2015-11-10 20:17:28 -0500

    added current_user check

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
+require 'test/unit'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     assert true
+   end
 end
